1f53fa9dfecac9fa0e746a6b26511967ee26b466
Nursan Omarov <Nursan.Omarov@audiit.com>
2026-04-11 19:47:00 +0500

    add

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,10 @@
+// rides.tsx
+import { View, Text } from "react-native";
+
+export default function Rides() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Rides</Text>
+    </View>
+  );
+}
